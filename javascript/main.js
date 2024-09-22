14f8417761edb61b1c45e9583267b7ba90b65245
EphemeralJosh <ephemeralstudiounity@gmail.com>
2024-09-22 14:30:14 -0300

    Main (Created basic structure to index page)

---
+++ javascript/main.js
@@ -0,0 +1,26 @@
+//Loader
+const loader = document.querySelector(".loader");
+
+window.addEventListener("load", () =>
+{
+    loader.classList.add("loader--hidden");
+
+    loader.addEventListener("transitionend", () =>
+    {
+        loader.remove();
+    })
+})
+
+const loader_images = document.querySelectorAll('.loader__image');
+
+let loader_images_index = 0;
+const loader_images_time = 200;
+
+
+function loader_images_animation() {
+    loader_images[loader_images_index].classList.remove('loader__image--active');
+    loader_images_index = (loader_images_index + 1) % loader_images.length;
+    loader_images[loader_images_index].classList.add('loader__image--active');
+}
+
+setInterval(loader_images_animation, loader_images_time);
